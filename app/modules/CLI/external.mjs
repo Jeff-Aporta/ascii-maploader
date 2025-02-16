67c884232318e145e2f4b9fd0df3d0fdfb89ea1f
jeff-aporta <jeffaporta@gmail.com>
2025-02-16 14:45:58 -0500

    [mod] agregar soporte para temas de CodeMirror y mejorar la función prettier_codemirror

--- app/modules/CLI/external.mjs
+++ app/modules/CLI/external.mjs
@@ -1,39 +1,83 @@
 import tree from "../treeAscii.mjs";
 import { writenodes } from "../html.mjs";
 
+let _codemirror_theme_name_ = "material-ocean";
+
 export default {
-    mui,
-    react,
-    react_mui,
-    fontawesome,
-    react_mui_fa,
-}
+  mui,
+  react,
+  react_mui,
+  fontawesome,
+  react_mui_fa,
+  prettier_codemirror,
+  codemirror_theme_name,
+};
 
 function react_mui_fa() {
-    react_mui();
-    fontawesome();
+  react_mui();
+  fontawesome();
 }
 
 function fontawesome() {
-    writenodes(
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    );
+  writenodes(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+  );
 }
 function react_mui() {
-    react();
-    mui();
+  react();
+  mui();
 }
 function react() {
-    tree("https://unpkg.com")
-        .add(
-            "react@18.3.1/umd/react.production.min.js",
-            "react-dom@18.3.1/umd/react-dom.production.min.js",
-            "@babel/standalone@7.25.5/babel.min.js"
-        )
-        .writehtml();
+  tree("https://unpkg.com")
+    .add(
+      "react@18.3.1/umd/react.production.min.js",
+      "react-dom@18.3.1/umd/react-dom.production.min.js",
+      "@babel/standalone@7.25.5/babel.min.js"
+    )
+    .writehtml();
 }
 function mui() {
-    tree("https://unpkg.com")
-        .add("@mui/material@5.16.7/umd/material-ui.production.min.js")
-        .writehtml();
+  tree("https://unpkg.com")
+    .add("@mui/material@5.16.7/umd/material-ui.production.min.js")
+    .writehtml();
+}
+
+function codemirror_theme_name(name) {
+  if (name) {
+    _codemirror_theme_name_ = name;
+  }
+  return _codemirror_theme_name_;
+}
+
+function prettier_codemirror(theme_name) {
+  if (theme_name) {
+    codemirror_theme_name(theme_name);
+  }
+  tree("https://")
+    .subDir("unpkg.com/prettier@2.8.8", (pretier) => {
+      pretier.js(
+        "standalone.js",
+        "parser-html.js",
+        "parser-postcss.js",
+        "parser-babel.js"
+      );
+    })
+    .subDir(
+      "cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.18",
+      (codemirror) => {
+        codemirror
+          .js("codemirror.min")
+          .css("codemirror.min.css", `theme/${codemirror_theme_name()}.min.css`)
+          .subDir("mode", (mode) => {
+            mode.js(
+              "xml/xml.min.js",
+              "javascript/javascript.min.js",
+              "css/css.min.js",
+              "sass/sass.min.js",
+              "htmlmixed/htmlmixed.min.js"
+            );
+          });
+      }
+    )
+    .writehtml();
 }
